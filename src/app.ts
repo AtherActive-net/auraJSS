abcef15cbe028ce14eb0a1e55a2aecf7c5398f53
AtherActive <ikbensalty@gmail.com>
2022-10-18 12:14:12 +0200

    fix: Several weird imports have been removed and are not exported

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,7 @@
-import color from './color.js';
-import flex from './styles/flex.js';
 import StyleSheet from './stylesheet.js';
-import fs from 'fs';
 import { compile } from './compiler.js';
 
 export {
     StyleSheet,
-    color,
-    flex,
     compile
 }
