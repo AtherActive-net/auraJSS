663dac5bb12b7ddebff5befe0fb870de7253bf09
AtherActive <ikbensalty@gmail.com>
2022-10-22 14:56:55 +0200

    fix: Broken imports

--- src/style.ts
+++ src/style.ts
@@ -15,7 +15,7 @@ import { border, borderBottom, borderTop, borderLeft, borderRight, borderCollaps
 import { flex } from "./styles/flex.js"
 import { font } from "./styles/font.js"
 import { grid } from "./styles/grid.js"
-import { listStyle } from "styles/list.js"
+import { listStyle } from "./styles/list.js"
 
 export {
     // basic
