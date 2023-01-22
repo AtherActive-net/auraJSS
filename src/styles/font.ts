26870ec3ffe8915f61b9c91985b6d0b63fb3d06e
AtherActive <ikbensalty@gmail.com>
2023-01-22 15:09:54 +0100

    fix: Fixed a bug where setting font({family}) was not working correctly
    When you attempted to set the family and ocmpiled, it would not represent it correctly in CSS. This has been resolved.

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -11,7 +11,7 @@ import { FontOptions, UnitValue } from "../interfaces.js";
 export function font(opts:FontOptions) {
     let out = {};
     if(opts.size) out['font-size'] = `${opts.size.v}${opts.size.u}`;
-    if(opts.family) out['font-family'] = opts.family;
+    if(opts.family) out['font-family'] = `'${opts.family}'`;
     if(opts.weight) out['font-weight'] = opts.weight;
     if(opts.style) out['font-style'] = opts.style;
     if(opts.variant) out['font-variant'] = opts.variant;
